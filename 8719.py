d4c0ee3bef97f24b2258223409ca0c68779e4831
Chanwoong1 <jcw7468@gmail.com>
2023-03-21 19:59:03 +0900

    feat: 3/21

--- 8719.py
+++ 8719.py
@@ -3,5 +3,8 @@ import sys
 input = sys.stdin.readline
 for _ in range(int(input())) :
     a, b = map(int, input().split())
-    if b % a == 0 : print("TAK")
-    else : print("NIE")
+    cnt = 0
+    while a < b :
+        a *= 2
+        cnt += 1
+    print(cnt)
